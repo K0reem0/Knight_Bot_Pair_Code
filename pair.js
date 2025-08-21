c3783edd9120043439359831a2da6f8de1442dd5
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-21 22:53:33 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -76,7 +76,7 @@ router.get('/', async (req, res) => {
                 if (connection === 'open') {  
                     console.log("✅ Connected successfully!");  
   
-                    await delay(10000);  
+                    await delay(15000);  
                       
                     try {  
                         const sessionKnight = fs.readFileSync(dirs + '/creds.json');  
